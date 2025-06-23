7a1b7b501d623776536f6f570b4650679bbbcec3
Lumintitus <sekarlumin@gmail.com>
2025-06-23 11:25:15 +0530

    changes lumin

--- src/Components/Buttonpanel.js
+++ src/Components/Buttonpanel.js
@@ -7,12 +7,12 @@ export default function Buttonpanel(props) {
                 <div className="container">
                     <div className="mx-w calculater">
                         <div className="number">
-                            <button className='button' onClick={() => { props.send("1") }}>1</button>
+                            <button className='button' onClick={() => { props.send("1") }}>5</button>
                             <button className='button' onClick={() => { props.send("2") }}>2</button>
                             <button className='button' onClick={() => { props.send("3") }}>3</button>
                             <button className='button' onClick={() => { props.send("clr") }}>clr</button>
                             <button className='button' onClick={() => { props.send("4") }}>4</button>
-                            <button className='button' onClick={() => { props.send("5") }}>5</button>
+                            <button className='button' onClick={() => { props.send("5") }}>1</button>
                             <button className='button' onClick={() => { props.send("6") }}>6</button>
                             <button className='button operator' onClick={() => { props.send("+") }}>+</button>
                             <button className='button' onClick={() => { props.send("7") }}>7</button>
